a4508fc9e539ccc6ba61f467803d1a3157fca71d
matheo.bienvenu <assasin787878@gmail.com>
2024-01-31 11:38:57 +0100

    ADD - Recuperation des data et hash :
    - Ajout de la fonction getHash pour retourner le hash nécessaire à l'API
    - Ajout de getData pour récupérer et filtrer les datas
    - Ajout du fichier apiKeys pour stocker la publicKey et privateKey

---
+++ src/api.js
@@ -0,0 +1,50 @@
+import fetch from 'node-fetch';
+import {createHash} from 'node:crypto'
+import {privateKey, publicKey} from "./apiKeys.js";
+
+/**
+ * Récupère les données de l'endpoint en utilisant les identifiants
+ * particuliers developer.marvels.com
+ * @param url l'end-point
+ * @return {Promise<json>}
+ */
+export const getData = async (url) => {
+    let filterResult
+    const timestamp = Date.now().toString()
+    const hash = await getHash(publicKey, privateKey, timestamp)
+    const image_size = "/portrait_xlarge.jpg"
+
+   const response = await fetch(url  + new URLSearchParams({
+        name: 'Hulk',
+        apikey: publicKey,
+        ts: timestamp,
+        hash: hash,
+    }))
+
+    const datas = await response.json()
+
+    filterResult = datas.data.results.filter(data => data.thumbnail.path !== "image_not_available")
+
+    return filterResult.map(data => {
+        return {
+            name: data.name,
+            description: data.description,
+            imageUrl: data.thumbnail.path + image_size
+        }
+    });
+}
+
+/**
+ * Calcul la valeur md5 dans l'ordre : timestamp+privateKey+publicKey
+ * cf documentation developer.marvels.com
+ * @param publicKey
+ * @param privateKey
+ * @param timestamp
+ * @return {Promise<ArrayBuffer>} en hexadecimal
+ */
+export const getHash = async (publicKey, privateKey, timestamp) => {
+    return createHash('md5').update(timestamp + privateKey + publicKey).digest("hex");
+}
+
+console.log(getData("https://gateway.marvel.com:443/v1/public/characters?").then((response) =>
+console.log("FILTERED VALUE : ", response)))
---
+++ src/apiKeys.js
@@ -0,0 +1,2 @@
+export const publicKey = "075187a7ec10942b53c1e2bd71497e46"
+export const privateKey = "e9e8b22c013d39ce56a66d440a25572b658db40d"
